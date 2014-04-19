91bc6bfa48337c81a9892e2d714635e6e501f6f6
Thad Rigby <thadrigby@gmail.com>
2014-04-19 17:44:40 -0600

    updated filter and added new submit option

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -6,6 +6,19 @@ angular.module('stoogeBloggerzApp')
 
       $scope.blogPosts = Blog.getPosts();
       $scope.blogAuthors = Authors.getAuthors();
-      $scope.filters = {};
+      $scope.theFilter = {};
+
+
+      $scope.allFilterClicked = function() {
+      	$scope.theFilter.author = " ";
+      }
+
+      $scope.submitNewPost = function() {
+      	var newPost = {};
+      	newPost.title = $scope.newPostTitle + " says...";
+      	newPost.body = $scope.newPostBody;
+      	newPost.author = $scope.newPostAuthor;
+      	$scope.blogPosts.push(newPost);
+      }
    
   });
